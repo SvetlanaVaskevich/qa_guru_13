75217e86f010695c8c4db31b186b111c5c5294e1
Svetlana Vaskevich <svaskevich@bk.ru>
2022-02-18 17:45:48 +0500

    TestBase added configuration

--- src/test/java/bk/vaskevich/fairtech/TestBase.java
+++ src/test/java/bk/vaskevich/fairtech/TestBase.java
@@ -7,6 +7,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
@@ -18,28 +19,28 @@ public class TestBase {
     public static void init(){
         SelenideLogger.addListener("AllureSelenide",new AllureSelenide());
 
-        /*String browser = System.getProperty("browser");
+        String browser = System.getProperty("browser");
         String version = System.getProperty("version");
         String login = System.getProperty("login","user1");
         String password = System.getProperty("password","1234");
         String url = System.getProperty("url");
-        String remoteUrl = "https://" + login + ":" + password + "@" + url;*/
+        String remoteUrl = "https://" + login + ":" + password + "@" + url;
 
         baseUrl = "https://fairtech.ru/";
         Configuration.browserSize = "1920x1080";
-        /*Configuration.browser = browser;
+        Configuration.browser = browser;
         Configuration.browserVersion = version;
         Configuration.remote = remoteUrl;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC",true);
         capabilities.setCapability("enableVideo",true);
-        Configuration.browserCapabilities = capabilities;*/
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
     public void addAttachments() {
-        //Attach.screenshotAs(System.getProperty("browser")+" "+System.getProperty("version"));
+        Attach.screenshotAs(System.getProperty("browser")+" "+System.getProperty("version"));
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
